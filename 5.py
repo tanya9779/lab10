1fc06bdb73630ef6a1f1179d8f76bfd8eb3c5d37
tanya9779 <tanya9779@mail.ru>
2015-11-03 12:03:12 +0300

    5

---
+++ 5.py
@@ -0,0 +1,26 @@
+ru_en=dict()
+in_f=open('input.txt')
+out_f=open('output.txt','w')
+s=in_f.readline().rstrip()
+while len(s)>0:
+    en,ru=list(s.split('\t=\t'))
+    if ',' in ru:
+        for i in ru.split(','):
+            i=i.lstrip()
+            if i in ru_en:
+
+                ru_en[i]=ru_en[i]+','+en
+            else:
+                ru_en[i]=en
+    else:
+        if ru in ru_en:
+            ru_en[ru]=ru_en[ru]+','+en
+        else:
+            ru_en[ru]=en
+
+    s=in_f.readline().rstrip()
+
+key_sort=sorted(ru_en.keys())
+for i in key_sort:
+    print ('\t-\t'.join((i,ru_en[i]) ),file=out_f)
+
